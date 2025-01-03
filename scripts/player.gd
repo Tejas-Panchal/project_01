2a4223892d5f6ee408495d288dee5854e12c019c
Tejas Panchal <tejaspanchal040@gmail.com>
2025-01-03 19:19:03 +0530

    added player and its movements
    gravity
    walk
    double jump

---
+++ scripts/player.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+
+var speed = 200
+var gravity = 1000
+var jump_velocity = -300
+var jump_count = 0
+var max_jump = 2
+
+@onready var body: AnimatedSprite2D = $Body
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	# add the gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		jump_count = 0
+	
+	# handle wall jump
+	if is_on_wall():
+		jump_count = 1
+	
+	# handle jump
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_velocity
+			jump_count = 1
+		elif jump_count < max_jump:
+			velocity.y = jump_velocity
+			jump_count += 1
+	
+	# get direction: (-1, 0, 1)
+	var direction = Input.get_axis("left", "right")
+	
+	# movement
+	if direction:
+		velocity.x = (direction * speed)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	# facing direction
+	if direction > 0:
+		body.flip_h = false
+	else:
+		body.flip_h = true
+	
+	# play animation
+	if is_on_floor():
+		if direction == 0:
+			body.play("idle")
+		else:
+			body.play("walk")
+	else:
+		body.play("jump")
+	move_and_slide()
